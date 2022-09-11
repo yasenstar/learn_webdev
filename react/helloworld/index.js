f789700840c27f4e25dc7adb9b924e47c683ef8a
Yasen <xiaoqizhao@outlook.com>
2022-09-11 12:48:25 -0400

    learn React for initial ReactDOM.render

--- react/helloworld/index.js
+++ react/helloworld/index.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1>Hello Everyone! Here is React.</h1>, document.getElementById("root"))
+//ReactDOM.render(<h1>Hello Everyone! Here is React.</h1>, document.getElementById("root"));
+//ReactDOM.render(<h1>Hello Everyone! Another Line</h1>, document.getElementById("root"));
+
+// ReactDOM.render(<h1>3+5</h1>, document.getElementById("root"));
+
+// ReactDOM.render(<p>Hi, my name is Yasen!</p>, document.getElementById("root"))
+
+// ReactDOM.render(<img src="./archi.png" width="250" height="50"></img>, document.getElementById("root"))
+
+ReactDOM.render(
+    <ul><img src="./archi.png" width="250" height="50"></img>new list
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+    </ul>,
+    document.getElementById("root")
+);
