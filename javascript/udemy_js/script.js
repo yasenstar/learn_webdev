1de78550c87147bccb94b5eecf1e188078eeb8c7
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2022-12-06 17:10:00 -0500

    update learning udemy

--- javascript/udemy_js/script.js
+++ javascript/udemy_js/script.js
@@ -2,3 +2,12 @@
 if (js === "amazing") alert("Javsscript is FUN!"); */
 
 console.log(40+8+23-10);
+
+let firstname = "Tom";
+console.log(firstname);
+
+let country = "China";
+let continent = "Asia";
+let population = 1300000000;
+
+console.log(country, continent, population);
